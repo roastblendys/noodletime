83a7c6ae6c5bab94813a9542d525c20705fbcd3a
hayashi <hayashi@hayashi-no-MacBook-Pro.local>
2015-06-25 13:13:22 +0900

    first commit

--- noodletimer/ViewController.swift
+++ noodletimer/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+// タイマーのラベル
+    @IBOutlet weak var time01: UILabel!
+// タイマーの設定
+    private var timer: NSTimer?
+    private var count: Int = 0
+    
+    private var min: Int = 2
+    private var sec: Int = 0
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    @IBAction func start_button(sender: AnyObject) {
+    }
 
+    
+    @IBAction func stop_button(sender: AnyObject) {
+    }
+    
+    @IBAction func reset_button(sender: AnyObject) {
+    }
+    
 }
 
