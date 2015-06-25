2ab1d934c83c6150f04150c8166879bbfaab5f83
hayashi <hayashi@hayashi-no-MacBook-Pro.local>
2015-06-25 16:25:04 +0900

    更新しました

--- noodletimer/ViewController.swift
+++ noodletimer/ViewController.swift
@@ -9,38 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
-// タイマーのラベル
+private let TIMER_INTERVAL: Double = 0.1
+    
+    // タイマーのラベル
     @IBOutlet weak var time01: UILabel!
+    
+    
 // タイマーの設定
     private var timer: NSTimer?
     private var count: Int = 0
-    private var min: Int = 2
+    private var min: Int = 3
     private var sec: Int = 0
-
-    @IBOutlet weak var stop_button:UIButton?
-    @IBOutlet weak var start_button:UIButton?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetTimer()
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+
+    func resetTimer() {
+        stopTimer()
+        min = 3
+        sec = 0
+        time01.text = timeStringGenerate()
+    }
     
+    func timeStringGenerate() -> String {
+        var secString: String = String(sec)
+        if (sec < 10) {
+            secString = "0" + secString
+        }
+        return (String(min) + ":" + secString)
+    }
+
+    
+    
+    func startTimer() {
+        stopTimer()
+        timer = NSTimer.scheduledTimerWithTimeInterval(TIMER_INTERVAL, target: self, selector: "onTick", userInfo: nil, repeats: true)
+    }
+    
+    
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    
+    func onTick() {
+        count++
+        if (count >= 10) {
+            countDown()
+            count = 0
+        }
+    }
+    
+    func countDown() {
+        sec--
+        if (sec < 0) {
+            min--
+            if (min < 0) {
+                stopTimer()
+            }
+            sec = 59
+        }
+        time01.text = timeStringGenerate()
+    }
 
     @IBAction func start_button(sender: AnyObject) {
+        startTimer()
     }
     
     @IBAction func stop_button(sender: AnyObject) {
+          stopTimer()
     }
     
     @IBAction func reset_button(sender: AnyObject) {
+        resetTimer()
     }
+    
     
 }
 
